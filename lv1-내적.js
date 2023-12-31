51ed67258e80baf3378fba7dceb19c149ba4812e
ChanGyun Lee <lgyn10@naver.com>
2023-12-31 15:46:44 +0900

    lodash zip함수 활용

--- lv1-내적.js
+++ lv1-내적.js
@@ -1,7 +1,14 @@
 // 70128
 
+const _ = require('lodash');
+
 function solution(a, b) {
   let ans = 0;
-  a.forEach((v, i) => (ans += v * b[i]));
+  let zippedArr = _.zip(a, b);
+  zippedArr.forEach(([v1, v2]) => (ans += v1 * v2));
   return ans;
 }
+
+let a = [1, 2, 3, 4];
+let b = [-3, -1, 0, 2];
+console.log(solution(a, b));
