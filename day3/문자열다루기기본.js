b62c32f20640b0414056bbade697da205e5cc849
ChanGyun Lee <lgyn10@naver.com>
2024-01-01 22:36:29 +0900

    day3 2차 수정

---
+++ day3/문자열다루기기본.js
@@ -0,0 +1,12 @@
+// 12918
+
+// function solution(s) {
+//   if ([4, 6].includes(s.length)) {
+//     return ![...s].every(isNaN);
+//   }
+//   return false;
+// }
+
+function solution(s) {
+  return [4, 6].includes(s.length) ? ([...s].some(isNaN) ? false : true) : false;
+}
--- 문자열다루기기본.js
+++
@@ -1,8 +0,0 @@
-// 12918
-
-function solution(s) {
-  if ([4, 6].includes(s.length)) {
-    return ![...s].every(isNaN);
-  }
-  return false;
-}
