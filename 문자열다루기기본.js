380cd7968fdd6d45c3db73b3f9e21b5becbbd61a
ChanGyun Lee <lgyn10@naver.com>
2024-01-01 18:17:59 +0900

    day3 1231일자 문제풀이 1차 수정

--- 문자열다루기기본.js
+++ 문자열다루기기본.js
@@ -2,7 +2,7 @@
 
 function solution(s) {
   if ([4, 6].includes(s.length)) {
-    return [...s].every((v) => !isNaN(v));
+    return ![...s].every(isNaN);
   }
   return false;
 }
--- 행렬의덧셈.js
+++ 행렬의덧셈.js
@@ -1,9 +1,23 @@
 // 12950
 
+// function solution(arr1, arr2) {
+//   return arr1.map((v1, i1) => v1.map((v2, i2) => v2 + arr2[i1][i2]));
+// }
+
+const _ = require('lodash');
+
 function solution(arr1, arr2) {
-  return arr1.map((v1, i1) => {
-    return v1.map((v2, i2) => {
-      return v2 + arr2[i1][i2];
-    });
-  });
+  let zippedArr = _.zip(arr1, arr2);
+  return zippedArr.map(([v1, v2]) => v1.map((v, i) => v + v2[i]));
 }
+
+let a1 = [
+  [1, 2],
+  [2, 3],
+];
+let a2 = [
+  [3, 4],
+  [5, 6],
+];
+
+console.log(solution(a1, a2));
