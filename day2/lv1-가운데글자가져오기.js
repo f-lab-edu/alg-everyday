27af3efaa1e0e0520ec5c1295771ca6fd9051ebd
ChanGyun Lee <lgyn10@naver.com>
2024-01-02 23:53:17 +0900

    day2 3차 수정

--- day2/lv1-가운데글자가져오기.js
+++ day2/lv1-가운데글자가져오기.js
@@ -2,6 +2,6 @@
 
 function solution(s) {
   let len = s.length;
-  let halfLen = Math.ceil(len / 2 - 1);
+  let halfLen = Math.floor((len - 1) / 2);
   return s.slice(halfLen, halfLen + (len % 2 ? 1 : 2));
 }
