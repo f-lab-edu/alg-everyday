09d40833825b403e3bdfe3387dc02452c53c3128
ChanGyun Lee <lgyn10@naver.com>
2024-01-03 00:07:23 +0900

    문자 가져오기 수정

--- day2/lv1-가운데글자가져오기.js
+++ day2/lv1-가운데글자가져오기.js
@@ -1,7 +1,13 @@
 // 12903
 
+// function solution(s) {
+//   let len = s.length;
+//   let halfLen = Math.floor((len - 1) / 2);
+//   return s.slice(halfLen, halfLen + (len % 2 ? 1 : 2));
+// }
+
 function solution(s) {
   let len = s.length;
-  let halfLen = Math.floor((len - 1) / 2);
-  return s.slice(halfLen, halfLen + (len % 2 ? 1 : 2));
+  let halfLen = Math.floor(len / 2);
+  return len % 2 ? s[halfLen] : s[halfLen - 1] + s[halfLen];
 }
