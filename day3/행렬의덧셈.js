9da4f8afc2caa0b0869b11fcac1854fb352e758f
ChanGyun Lee <lgyn10@naver.com>
2024-01-03 00:23:35 +0900

    day3 3차 수정

--- day3/행렬의덧셈.js
+++ day3/행렬의덧셈.js
@@ -4,11 +4,16 @@
 //   return arr1.map((v1, i1) => v1.map((v2, i2) => v2 + arr2[i1][i2]));
 // }
 
+// function solution(arr1, arr2) {
+//   let zippedArr = _.zipWith(arr1, arr2);
+//   return zippedArr.map(([v1, v2]) => v1.map((v, i) => v + v2[i]));
+// }
+
 const _ = require('lodash');
 
 function solution(arr1, arr2) {
-  let zippedArr = _.zip(arr1, arr2);
-  return zippedArr.map(([v1, v2]) => v1.map((v, i) => v + v2[i]));
+  let zippedArr = _.zipWith(arr1, arr2);
+  return zippedArr.map(([v1, v2]) => _.zipWith(v1, v2, (a, b) => a + b));
 }
 
 let a1 = [
