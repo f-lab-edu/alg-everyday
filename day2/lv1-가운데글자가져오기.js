094ae55e1763ce2c78ae4df64059014288a1c86d
ChanGyun Lee <lgyn10@naver.com>
2024-01-04 19:50:01 +0900

    가운데글자가져오기: 수정

--- day2/lv1-가운데글자가져오기.js
+++ day2/lv1-가운데글자가져오기.js
@@ -6,8 +6,14 @@
 //   return s.slice(halfLen, halfLen + (len % 2 ? 1 : 2));
 // }
 
+// function solution(s) {
+//   let len = s.length;
+//   let halfLen = Math.floor(len / 2 - 1);
+//   return len % 2 ? s[halfLen] : s[halfLen - 1] + s[halfLen];
+// }
+
 function solution(s) {
   let len = s.length;
-  let halfLen = Math.floor(len / 2);
-  return len % 2 ? s[halfLen] : s[halfLen - 1] + s[halfLen];
+  let mid = Math.floor((len - 1) / 2);
+  return s.slice(mid, mid + 1 + !(len % 2));
 }
