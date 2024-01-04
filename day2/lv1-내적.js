02d8fd3a8d2ddb576c80a6bdaf0e6d17ad555ba5
ChanGyun Lee <lgyn10@naver.com>
2024-01-04 19:23:47 +0900

    내적: sum, zipWith 함수정리

--- day2/lv1-내적.js
+++ day2/lv1-내적.js
@@ -2,10 +2,14 @@
 
 const _ = require('lodash');
 
-function solution(a, b) {
-  let zippedArr = _.zip(a, b);
-  return zippedArr.map(([v1, v2]) => v1 * v2).reduce((acc, curVal) => acc + curVal, 0);
-}
+// function solution(a, b) {
+//   let zippedArr = _.zip(a, b);
+//   return zippedArr.map(([v1, v2]) => v1 * v2).reduce((acc, curVal) => acc + curVal, 0);
+// }
+
+const solution = (a, b) => {
+  return _.sum(_.zipWith(a, b, (v1, v2) => v1 * v2));
+};
 
 let a = [1, 2, 3, 4];
 let b = [-3, -1, 0, 2];
