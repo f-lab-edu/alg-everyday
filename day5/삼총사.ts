9e56fa520d4a69868537265c96a3e791839b6ed4
ChanGyun Lee <lgyn10@naver.com>
2024-01-06 17:54:33 +0900

    시저암호: return 플로우 앞으로

--- day5/삼총사.ts
+++ day5/삼총사.ts
@@ -4,10 +4,10 @@ function 삼총사(num: number[]) {
   const arr: number[][] = [];
   num.forEach((v1) =>
     num.forEach((v2) =>
-      num.forEach(
-        (v3) =>
-          v1 + v2 + v3 === 0 && arr.push([v1, v2, v3].sort((a, b) => a - b)),
-      ),
+      num.forEach((v3) => {
+        if (v1 + v2 + v3 === 0)
+          return arr.push([v1, v2, v3].sort((a, b) => a - b));
+      }),
     ),
   );
   return _.uniqWith(arr, _.isEqual).length;
--- day5/시저암호.ts
+++ day5/시저암호.ts
@@ -17,6 +17,24 @@ function caesarCipher1(s: string, n: number) {
 }
 
 // 시저 암호 함수 2
+// function caesarCipher2(s: string, n: number) {
+//   const space_ASCII = 32;
+//   const A_ASCII = 65;
+//   const a_ASCII = 97;
+//   const azNum = 26;
+
+//   return [...s]
+//     .map((v) => {
+//       let basisNum: number;
+//       const ascNumSum = v.charCodeAt(0) + n;
+//       if (v.match(/[A-Z]/g)) basisNum = A_ASCII;
+//       else if (v.match(/[a-z]/g)) basisNum = a_ASCII;
+//       else return space_ASCII;
+//       return basisNum + ((ascNumSum - basisNum) % azNum);
+//     })
+//     .map((v) => String.fromCharCode(v))
+//     .join("");
+// }
 function caesarCipher2(s: string, n: number) {
   const space_ASCII = 32;
   const A_ASCII = 65;
@@ -27,16 +45,18 @@ function caesarCipher2(s: string, n: number) {
     .map((v) => {
       let basisNum: number;
       const ascNumSum = v.charCodeAt(0) + n;
-      if (v.match(/[A-Z]/g)) basisNum = A_ASCII;
-      else if (v.match(/[a-z]/g)) basisNum = a_ASCII;
-      else return space_ASCII;
-
-      return basisNum + ((ascNumSum - basisNum) % azNum);
+      if (v.charCodeAt(0) === space_ASCII) return space_ASCII;
+      if (v.match(/[A-Z]/g)) {
+        basisNum = A_ASCII;
+        return basisNum + ((ascNumSum - basisNum) % azNum);
+      } else if (v.match(/[a-z]/g)) {
+        basisNum = a_ASCII;
+        return basisNum + ((ascNumSum - basisNum) % azNum);
+      } else return space_ASCII;
     })
     .map((v) => String.fromCharCode(v))
     .join("");
 }
-
 const str = "a B z";
 console.log(caesarCipher1(str, 4)); // e F d
 console.log(caesarCipher2(str, 4)); // e F d
