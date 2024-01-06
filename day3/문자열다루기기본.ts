89eff627688feb7754e9e2f3780b8ab46a6af51e
ChanGyun Lee <lgyn10@naver.com>
2024-01-06 16:17:46 +0900

    문자열다루기기본: 리펙토링

--- day3/문자열다루기기본.ts
+++ day3/문자열다루기기본.ts
@@ -7,6 +7,24 @@
 //   return false;
 // }
 
+// declare.ts에서 선언한 _를 가져오지 못함..
+const lo = require("lodash");
+
 function 문자열다루기기본(s: string) {
-  return [4, 6].includes(s.length) && [...s].some((v: any) => isNaN(v));
+  return (
+    [4, 6].includes(s.length) &&
+    [...s].every((v: string) => lo.isInteger(parseInt(v, 10)))
+  );
 }
+
+const s1 = "a234";
+const s2 = "1234";
+
+console.log(문자열다루기기본(s1));
+console.log(문자열다루기기본(s2));
+
+// isNumber와 isInteger
+const s = "a";
+console.log(parseInt(s, 10)); // NaN
+console.log(lo.isNumber(parseInt(s, 10))); // true
+console.log(lo.isInteger(parseInt(s, 10))); // false
