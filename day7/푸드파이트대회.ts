0789bb0fc886afccfb0e06b2c409c8046ba41d1e
ChanGyun Lee <lgyn10@naver.com>
2024-01-09 15:30:59 +0900

    푸드파이트대회: 리펙토링

--- day7/푸드파이트대회.ts
+++ day7/푸드파이트대회.ts
@@ -2,18 +2,22 @@
 
 const 푸드파이트대회 = (food: number[]) => {
   const usableFoodForHalf = food.map((v) => Math.floor(v / 2));
+  console.log(usableFoodForHalf); // [0,3,0,1]
+  console.log([...usableFoodForHalf.keys()]); // [0,1,2,3]
   const foodArr = [];
 
-  for (const foodIdx in usableFoodForHalf) {
-    const foodCnt = usableFoodForHalf[foodIdx];
-    foodCnt && foodArr.push(foodIdx.repeat(foodCnt));
+  for (const idx of usableFoodForHalf.keys()) {
+    // 단순 if문
+    if (usableFoodForHalf[idx])
+      foodArr.push(String(idx).repeat(usableFoodForHalf[idx]));
   }
-
-  return foodArr.join("") + "0" + foodArr.reverse().join("");
+  return [...foodArr, 0, ...foodArr.reverse()].join("");
 };
 
 const food1 = [1, 3, 4, 6];
-const food2 = [1, 7, 1, 2];
-
 console.log(푸드파이트대회(food1)); // "1223330333221"
+const food2 = [1, 7, 1, 2];
 console.log(푸드파이트대회(food2)); // "111303111"
+
+// 변경할 때 && 피하자, 리턴값을 활용할 때만 쓰자
+// usableFoodForHalf.keys() 사용
