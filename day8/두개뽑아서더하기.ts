c71e13da78748832245173d3659c63f226684693
ChanGyun Lee <lgyn10@naver.com>
2024-01-11 20:56:25 +0900

    두개뽑아서더하기: flatMap 함수 적용

--- day8/두개뽑아서더하기.ts
+++ day8/두개뽑아서더하기.ts
@@ -2,13 +2,23 @@
 
 import { _ } from "../declare";
 
+// const 두개뽑아서더하기 = (numbers: number[]): number[] => {
+//   const arr: number[] = [];
+//   for (const key of numbers.keys()) {
+//     const exceptKeyNums = numbers.filter((_, i) => i !== key);
+//     exceptKeyNums.forEach((v) => arr.push(numbers[key] + v));
+//   }
+//   return _.uniq(arr).sort((a: number, b: number) => a - b);
+// };
+
+// flatMap 함수
+// 중첩 배열을 평탄화하는데 사용
 const 두개뽑아서더하기 = (numbers: number[]): number[] => {
-  const arr: number[] = [];
+  const arr: number[][] = [];
   for (const key of numbers.keys()) {
-    const exceptKeyNums = numbers.filter((_, i) => i !== key);
-    exceptKeyNums.forEach((v) => arr.push(numbers[key] + v));
+    arr.push(numbers.filter((_, i) => i !== key).map((v) => numbers[key] + v));
   }
-  return _.uniq(arr).sort((a: number, b: number) => a - b);
+  return _.uniq(arr.flatMap((x) => x)).sort((a: number, b: number) => a - b);
 };
 
 const n1 = [2, 1, 3, 4, 1];
