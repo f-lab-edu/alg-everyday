14b37f38857c58c102c940ebd0e3ea22b5243eef
ChanGyun Lee <lgyn10@naver.com>
2024-01-12 23:28:20 +0900

    모의고사: 리펙토링

--- day10/모의고사.ts
+++ day10/모의고사.ts
@@ -7,9 +7,9 @@ const student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 const 모의고사 = (answers: number[]): number[] => {
   const ansCnts = [0, 0, 0];
   answers.forEach((v, i) => {
-    if (v === student1[i % student1.length]) ansCnts[0]++;
-    if (v === student2[i % student2.length]) ansCnts[1]++;
-    if (v === student3[i % student3.length]) ansCnts[2]++;
+    [student1, student2, student3].forEach((stu, idx) => {
+      if (v === stu[i % stu.length]) ansCnts[idx]++;
+    });
   });
   return ansCnts
     .map((v, i) => (v === Math.max(...ansCnts) ? i + 1 : 0))
