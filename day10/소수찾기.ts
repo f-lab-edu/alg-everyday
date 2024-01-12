c0c9d173c2a588a5a1e3f1820b75d42d147267af
ChanGyun Lee <lgyn10@naver.com>
2024-01-12 21:04:59 +0900

    소수찾기:효율성 만족위한 리펙토링

--- day10/소수찾기.ts
+++ day10/소수찾기.ts
@@ -1,15 +1,22 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12921
 
 const 소수찾기 = (n: number): number => {
-  const primeArr = [];
+  // 에라토스테네스의 체
+  // 배수를 지워나간다
+  const totalArr = Array(n + 1).fill(true);
+  totalArr[0] = false;
+  totalArr[1] = false;
   for (let i = 2; i <= n; i++) {
-    let isPrime = true;
-    for (let j = 2; j <= Math.sqrt(i); j++) {
-      if (i % j === 0) isPrime = false;
+    if (totalArr[i] === true) {
+      for (let j = i + i; j <= n; j += i) {
+        // i + i 의 의미는 자기 자신은 지우지 않는 것
+        totalArr[j] = false;
+      }
     }
-    if (isPrime === true) primeArr.push(i);
   }
-  return primeArr.length;
+
+  console.log(totalArr);
+  return totalArr.filter((v) => v).length;
 };
 
 const day10n1 = 10;
