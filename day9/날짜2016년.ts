341909a636e4f457277c293030f20be9d30c8615
ChanGyun Lee <lgyn10@naver.com>
2024-01-15 15:47:06 +0900

    날짜2016년: Date() 객체 미사용 버전

--- day9/날짜2016년.ts
+++ day9/날짜2016년.ts
@@ -1,22 +1,30 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12901
 // 윤년임
 
-// O(n)
-// const whatDay = (month: number, day: number): string => {
-//   const daysPerMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-//   const dayString = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
-//   const days = _.sum(daysPerMonth.slice(0, month - 1)) + day;
-//   return dayString[days % 7 ? (days % 7) - 1 : 6];
-// };
+import { _ } from "../declare";
 
-// O(k)
+// O(n)
 const whatDay = (month: number, day: number): string => {
-  return (new Date("2016-" + month + "-" + day) + "")
-    .split(" ")[0]
-    .toUpperCase();
+  const daysPerMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const dayString = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
+  const days = _.sum(daysPerMonth.slice(0, month - 1)) + day;
+  return dayString[((days % 7) + 6) % 7];
+  // return dayString[days % 7 ? (days % 7) - 1 : 6];
 };
 
-// const [month, day] = [5, 24];
-const [month, day] = [9, 1];
+// O(k);
+// const whatDay = (month: number, day: number): string => {
+//   return (new Date("2016-" + month + "-" + day) + "")
+//     .split(" ")[0]
+//     .toUpperCase();
+// };
 
-console.log(whatDay(month, day)); // "TUE"
+const [day9month1, day9day1] = [5, 24];
+const [day9month2, day9day2] = [9, 1];
+const [day9month3, day9day3] = [1, 6];
+const [day9month4, day9day4] = [3, 3];
+
+console.log(whatDay(day9month1, day9day1)); // "TUE"
+console.log(whatDay(day9month2, day9day2)); // "THU"
+console.log(whatDay(day9month3, day9day3)); // "WED"
+console.log(whatDay(day9month4, day9day4)); // "THU"
