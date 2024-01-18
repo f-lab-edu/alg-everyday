753ef7d97e3f89d83e4c2d0cca200b72fac100b7
ChanGyun Lee <lgyn10@naver.com>
2024-01-18 17:18:20 +0900

    range, min함수 정정

--- day10/기사단원의무기.ts
+++ day10/기사단원의무기.ts
@@ -1,6 +1,8 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/136798
-import { _ } from "../declare";
-// 시간 초과
+
+import { min, range, sum } from "lodash";
+
+//! 시간 초과
 // const 기사단원의무기 = (
 //   number: number,
 //   limit: number,
@@ -18,27 +20,26 @@ import { _ } from "../declare";
 //   return _.sum(divisorNums);
 // };
 
-// 효율성 개선 - let 쓰기
+//! 효율성 개선
 const 기사단원의무기 = (
   number: number,
   limit: number,
   power: number,
 ): number => {
-  const divisorNums = _.range(0, number, 0).map(
-    (_: number, i: number): number => {
-      const number = i + 1;
-      const sqrtNumber = Math.sqrt(number);
+  const divisorNums = range(1, number + 1).map(
+    (attackPoint: number): number => {
+      const APSqrt = Math.sqrt(attackPoint);
       let cnt: number = 0;
-      for (let j = 0; j <= sqrtNumber; j++) {
-        if (number % j === 0) cnt += 2;
+      for (let j = 0; j <= APSqrt; j++) {
+        if (attackPoint % j === 0) cnt += 2;
       }
-      if (Math.floor(sqrtNumber) === sqrtNumber) cnt -= 1;
-      return Math.min(cnt, limit) === cnt ? cnt : power;
+      if (Math.floor(APSqrt) === APSqrt) cnt -= 1;
+      return min([cnt, limit]) === cnt ? cnt : power;
       // (1) return cnt > limit ? power : cnt;
       // (2) return _.min([cnt, limit]) === cnt ? cnt : power;
     },
   );
-  return _.sum(divisorNums);
+  return sum(divisorNums);
 };
 
 const [day10number1, day10limit1, day10power1] = [5, 3, 2];
