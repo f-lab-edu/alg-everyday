ad9a8653b43d2d3b5991e7e0daa0935800662cb5
ChanGyun Lee <lgyn10@naver.com>
2024-01-21 00:20:47 +0900

    소수찾기: 시간복잡도, 공간복잡도 풀이 시간 순으로 정리

--- day10/소수찾기.ts
+++ day10/소수찾기.ts
@@ -1,26 +1,9 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12921
 
-//! (1) 시간 효율성 ok, 공간 효율성 fail
-// 시간복잡도: O(n log(log n))
-// 공간복잡도: O(n)
-const 소수찾기1 = (n: number): number => {
-  const totalArr = Array(n + 1).fill(true);
-  totalArr[0] = false;
-  totalArr[1] = false;
-  for (let i = 2; i <= n; i++) {
-    if (totalArr[i] === true) {
-      for (let j = i + i; j <= n; j += i) {
-        totalArr[j] = false;
-      }
-    }
-  }
-  return totalArr.filter((v) => v).length;
-};
-
-//! (2) 시간 효율성 fail, 공간 효율성 ok
+//! (1) 시간 효율성 fail, 공간 효율성 ok
 // 시간복잡도: O(n√n)
 // 공간복잡도: O(1)
-const 소수찾기2 = (n: number) => {
+const 소수찾기1 = (n: number) => {
   let cnt = 0;
   for (let i = 2; i <= n; i++) {
     let isPrime = true;
@@ -34,6 +17,23 @@ const 소수찾기2 = (n: number) => {
     }
   }
   return cnt;
+};
+
+//! (2) 시간 효율성 ok, 공간 효율성 fail
+// 시간복잡도: O(n log(log n))
+// 공간복잡도: O(n)
+const 소수찾기2 = (n: number): number => {
+  const totalArr = Array(n + 1).fill(true);
+  totalArr[0] = false;
+  totalArr[1] = false;
+  for (let i = 2; i <= n; i++) {
+    if (totalArr[i] === true) {
+      for (let j = i + i; j <= n; j += i) {
+        totalArr[j] = false;
+      }
+    }
+  }
+  return totalArr.filter((v) => v).length;
 };
 
 //! (3) 시간 효율성 ok, 공간 효율성 fail
@@ -68,22 +68,6 @@ const 소수찾기4 = (n: number) => {
   return primes.length;
 };
 
-//! (5) break 활용
-// 시간복잡도: O(n√n)
-// 공간복잡도: O(1)
-const 소수찾기5 = (n: number) => {
-  let nonPrimeCnt = 0;
-  for (let i = 2; i <= n; i++) {
-    for (let j = 2; j <= Math.sqrt(i); j++) {
-      if (i % j === 0) {
-        nonPrimeCnt++;
-        break;
-      }
-    }
-  }
-  return n - nonPrimeCnt - 1;
-};
-
 const day10n1 = 10;
 const day10n2 = 5;
 
@@ -98,6 +82,3 @@ console.log(소수찾기3(day10n2)); // 3
 
 console.log(소수찾기4(day10n1)); // 4
 console.log(소수찾기4(day10n2)); // 3
-
-console.log(소수찾기5(day10n1)); // 4
-console.log(소수찾기5(day10n2)); // 3
