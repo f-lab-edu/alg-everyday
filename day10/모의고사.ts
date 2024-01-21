ef8a4016d1c5977259342d2c3405d87eebab3136
ChanGyun Lee <lgyn10@naver.com>
2024-01-22 01:19:59 +0900

    모의고사: sum함수로 불필요한 변수 선언 제거

--- day10/모의고사.ts
+++ day10/모의고사.ts
@@ -1,34 +1,24 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42840
 
+import { sum } from "lodash";
+
 const student1 = [1, 2, 3, 4, 5];
 const student2 = [2, 1, 2, 3, 2, 4, 2, 5];
 const student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-// const 모의고사 = (answers: number[]): number[] => {
-//   // const ansCnts = [0, 0, 0];
-//   // answers.forEach((v, i) => {
-//   //   [student1, student2, student3].forEach((stu, idx) => {
-//   //     if (v === stu[i % stu.length]) ansCnts[idx]++;
-//   //   });
-//   // });
-//   return ansCnts
-//     .map((v, i) => (v === Math.max(...ansCnts) ? i + 1 : 0))
-//     .filter((v) => v !== 0);
-// };
-
 const 모의고사 = (answers: number[]): number[] => {
-  const ansCnts = [student1, student2, student3].map((student) => {
-    let ansCnt = 0;
-    answers.forEach((ans, ansNo) => {
-      if (ans === student[ansNo % student.length]) ansCnt++;
-    });
-    return ansCnt;
-  });
-
+  const ansCnts = [student1, student2, student3].map((student) =>
+    sum(
+      answers.map((ans, ansNo) =>
+        ans === student[ansNo % student.length] ? 1 : 0,
+      ),
+    ),
+  );
   return ansCnts
     .map((v, i) => (v === Math.max(...ansCnts) ? i + 1 : 0))
     .filter((v) => v !== 0);
 };
+
 const d10answers1 = [1, 2, 3, 4, 5];
 const d10answers2 = [1, 3, 2, 4, 2];
 
