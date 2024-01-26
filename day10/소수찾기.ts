1cd337558b5bdca9f2902bc0c8447b58bee128ae
ChanGyun Lee <lgyn10@naver.com>
2024-01-26 19:59:19 +0900

    소수찾기: 드디어 최종인가ㅏㅏㅏㅏㅏ

--- day10/소수찾기.ts
+++ day10/소수찾기.ts
@@ -59,7 +59,7 @@ const 소수찾기3 = (n: number) => {
 
 const 소수찾기4 = (n: number) => {
   const primes: number[] = [];
-  // 2부터 n까지 순회하면서 ---------------------- 여기서 시간 잡아먹는 중
+  // 2부터 n까지 순회하면서 ---------------------- 여기서 시간 잡아먹는 중 -> 아님 ㅇㅇ (3)에서도 한번은 돌아야함
   for (let i = 2; i <= n; i++) {
     // 임시 변수인 isPrime를 true로 선언
     let isPrime = true;
@@ -78,13 +78,27 @@ const 소수찾기4 = (n: number) => {
 
 //! (5) 두번째 순회에서 primes의 모든 요소 순회 방지
 //! 를 통한 시간 효율성 개선
-const 소수찾기5 = (n: number) => {
+//! +) primes 배열이 target의 제곱근 수까지만 유지되면 된다.
+const 소수찾기5 = (num: number) => {
   const primes: number[] = [];
+  const numSqrt = Math.sqrt(num);
+  let primesToNumSqrt: number[] = [];
 
-  for (let target = 2; target <= n; target++) {
-    // some() 메서드로 primes의 모든 요소를 순회하는 것을 방지
+  for (let target = 2; target <= num; target++) {
+    // 기존 primes.some()은 모든 prime에 대해서 검사를 진행했다.
+    // primesToNumSqrt를 도입해서 num의 제곱근까지의 소수만을 저장했다.
+    // 즉, primesToNumSqrt.some()은 num의 제곱근까지만의 소수를 검사하기 때문에 시간 효율성이 극도로 좋아진다.
+
+    // primes[primes.length - 1] <= numSqrt를 통해 불필요한 filter 메서드 동작을 제한했다.
+    // 이 조건 연산은 시간적으로 영향이 거의 없다 (이 연산을 쓰는 것이 이득이다.)
+    if (primes[primes.length - 1] <= numSqrt) {
+      primesToNumSqrt = primes.filter((prime) => prime <= numSqrt);
+    }
+
+    // some() 메서드로 primesToNumSqrt 모든 요소를 순회하는 것을 방지
     // true 조건이 만족하는 즉시 반환
-    if (!primes.some((prime) => target % prime === 0)) primes.push(target);
+    if (!primesToNumSqrt.some((prime) => target % prime === 0))
+      primes.push(target);
   }
   return primes.length;
 };
