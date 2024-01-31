37b8e0056fc4805c969d2a9ce0166db61ca36f72
ChanGyun Lee <lgyn10@naver.com>
2024-01-31 20:59:35 +0900

    올바른괄호: add

---
+++ day12/올바른괄호.ts
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+import { sum } from "lodash";
+
+const 올바른괄호 = (s: string): boolean => {
+  if (s.charAt(0) === ")") return false;
+  return !sum([...s].map((str) => (str === "(" ? 1 : -1)));
+};
+
+const alls1 = "()()";
+const alls2 = "(())()";
+const alls3 = ")()(";
+const alls4 = "(()(";
+
+console.log(올바른괄호(alls1)); // true
+console.log(올바른괄호(alls2)); // true
+console.log(올바른괄호(alls3)); // false
+console.log(올바른괄호(alls4)); // false
