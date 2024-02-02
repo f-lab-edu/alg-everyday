7fbfd9a8c9566297da61c15166a66dcc26f9521e
ChanGyun Lee <lgyn10@naver.com>
2024-02-02 12:19:36 +0900

    chore: 파일 정리

--- day12/reg.js
+++
@@ -1,7 +0,0 @@
-const lodashWords = (str) => str.split(/(?<=\D)(?=\d)|_|(?<=\d)(?=\D)|\s/);
-
-console.log(lodashWords("F-5 Freedom Fighter"));
-console.log(lodashWords("img1.png"));
-
-const str = " ";
-console.log(parseInt(str, 10));
