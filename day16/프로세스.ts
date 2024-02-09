1ca88264545c9322c57b33e13b272fa98894c1b3
ChanGyunLee <lgyn10@naver.com>
2024-02-09 18:35:41 +0900

    프로세스: add

---
+++ day16/프로세스.ts
@@ -0,0 +1,47 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42587
+
+import { clone, map, maxBy } from "lodash";
+
+// 특정 프로세스가 몇 번째로 실행되는지 알아낸다
+
+//| 1. 실행 대기 큐(Queue)에서 대기중인 프로세스 하나를 꺼냅니다.
+//| 2. 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면 방금 꺼낸 프로세스를 다시 큐에 넣습니다.
+//| 3. 만약 그런 프로세스가 없다면 방금 꺼낸 프로세스를 실행합니다.
+//| 3-1. 한 번 실행한 프로세스는 다시 큐에 넣지 않고 그대로 종료됩니다.
+
+// priorities : 실행 대기 큐에 있는 프로세스의 중요도가 순서대로 담긴 배열
+// 1 <= priority <= 9
+
+// location : 몇 번째로 실행되는지 알고싶은 프로세스의 위치 (idx : location -1 )
+// 0 <= loc <= priorities.length - 1
+// 하나 남으면, 남은 하나 실행하면 된다.
+
+const 프로세스 = (priorities: number[], location: number): number => {
+  //! [priority, location] 튜플 구조 배열 선언
+  const tuplesQueue = map(priorities, (priority, idx) => {
+    const loc = idx + 1;
+    return [priority, loc];
+  });
+  const tuplesQueueForIndexOf = clone(tuplesQueue); // 얕은 복사로도 조건 충족
+
+  //! 실행 순서가 정렬될 큐 선언
+  const orderedQueue: number[][] = [];
+
+  //! 코어 로직
+  while (tuplesQueue.length > 0) {
+    const maxQueue = maxBy(tuplesQueue, 0)![0];
+    const elementToBoConfirmed = tuplesQueue.shift();
+    (elementToBoConfirmed![0] === maxQueue ? orderedQueue : tuplesQueue).push(
+      elementToBoConfirmed!,
+    );
+  }
+  // console.log(orderedQueue);
+  // console.log(tuplesQueueForIndexOf[location]);
+  return orderedQueue.indexOf(tuplesQueueForIndexOf[location]) + 1;
+};
+
+const [d16priorities1, d16location1] = [[2, 1, 3, 2], 2];
+const [d16priorities2, d16location2] = [[1, 1, 9, 1, 1, 1], 0];
+
+console.log(프로세스(d16priorities1, d16location1)); // 1
+console.log(프로세스(d16priorities2, d16location2)); // 5
