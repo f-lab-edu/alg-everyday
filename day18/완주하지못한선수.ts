6e9cec16f9afb3dfd492f8a2252f413ca68e0863
ChanGyun Lee <lgyn10@naver.com>
2024-03-07 17:07:55 +0900

    완주하지못한선수 : cntNameMapping 함수가 하나의 이름에 동작하는 함수로 전환

--- day18/완주하지못한선수.ts
+++ day18/완주하지못한선수.ts
@@ -11,7 +11,7 @@ import { difference } from 'lodash';
 // 참가자 이름음 알파벳 소문자
 // 동명이인이 있을 수 있다 (다수) -> 이 부분을 처리하는 것이 관건
 
-//! 실행 함수
+// //! 1st ver. 실행 함수
 const uncompletedRunner = (runners: string[], cmpRunners: string[]): string => {
   //! 동명이인이 없는 경우
   // lodash 연습용. 비즈니스 로직에서는 공간만 차지할 수도...?!
@@ -20,10 +20,9 @@ const uncompletedRunner = (runners: string[], cmpRunners: string[]): string => {
   if (diff.length !== 0) return diff[0];
 
   //! 동명이인이 있는 경우
-
   //| {이름 -> 이름 수}로 매핑하는 함수
   const cntNameMapping = (map: Map<string, number>, names: string[]): void => {
-    names.forEach(name => map.set(name, (map.get(name) || 0) + 1));
+    names.forEach(name => map.set(name, (map.get(name) ?? 0) + 1));
   };
 
   //! 코어 로직
@@ -31,6 +30,33 @@ const uncompletedRunner = (runners: string[], cmpRunners: string[]): string => {
   const cmpRunnersMap = new Map();
   cntNameMapping(runnersMap, runners);
   cntNameMapping(cmpRunnersMap, cmpRunners);
+  for (const runner of runners) {
+    if (runnersMap.get(runner) !== cmpRunnersMap.get(runner)) return runner;
+  }
+
+  // 이 코드에 도달하기 전에 리턴값은 확실하게 존재한다.
+  throw new Error('string 타입이 반환되지 않았습니다.');
+};
+
+//! 2nd ver. 실행 함수
+const uncompletedRunner2 = (runners: string[], cmpRunners: string[]): string => {
+  //! 동명이인이 없는 경우
+  // lodash 연습용. 비즈니스 로직에서는 공간만 차지할 수도...?!
+  // 하지만, 가독성은 제일 좋고 조건 하에 결과도 확실하다.
+  const diff = difference(runners, cmpRunners);
+  if (diff.length !== 0) return diff[0];
+
+  //! 동명이인이 있는 경우
+  //| {이름 -> 이름 수}로 매핑하는 함수
+  const cntNameMapping = (map: Map<string, number>, name: string): void => {
+    map.set(name, (map.get(name) ?? 0) + 1);
+  };
+
+  //! 코어 로직
+  const runnersMap = new Map();
+  const cmpRunnersMap = new Map();
+  runners.forEach(name => cntNameMapping(runnersMap, name));
+  cmpRunners.forEach(name => cntNameMapping(cmpRunnersMap, name));
   for (const runner of runners) {
     if (runnersMap.get(runner) !== cmpRunnersMap.get(runner)) return runner;
   }
@@ -60,3 +86,8 @@ console.log(uncompletedRunner(runners1, cmpRunners1)); // 'leo'
 console.log(uncompletedRunner(runners2, cmpRunners2)); // 'vinko'
 console.log(uncompletedRunner(runners3, cmpRunners3)); // 'mislav'
 console.log(uncompletedRunner(runners4, cmpRunners4)); // 'ana'
+console.log('---');
+console.log(uncompletedRunner2(runners1, cmpRunners1)); // 'leo'
+console.log(uncompletedRunner2(runners2, cmpRunners2)); // 'vinko'
+console.log(uncompletedRunner2(runners3, cmpRunners3)); // 'mislav'
+console.log(uncompletedRunner2(runners4, cmpRunners4)); // 'ana'
