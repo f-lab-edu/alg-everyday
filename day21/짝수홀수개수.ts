48ee03bf06ab309014b69e246dfc374a545141b0
ChanGyun Lee <lgyn10@naver.com>
2024-03-10 17:04:33 +0900

    짝수홀수개수: add

---
+++ day21/짝수홀수개수.ts
@@ -0,0 +1,16 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120824
+
+import { forEach } from 'lodash';
+
+const 짝수홀수개수 = (numList: number[]): number[] => {
+  let evenCnt = 0;
+  let oddCnt = 0;
+  forEach(numList, (n) => (n % 2 === 0 ? evenCnt++ : oddCnt++));
+  return [evenCnt, oddCnt];
+};
+
+const nl1 = [1, 2, 3, 4, 5];
+const nl2 = [1, 3, 5, 7];
+
+console.log(짝수홀수개수(nl1));
+console.log(짝수홀수개수(nl2));
