14a25444602c4603e643c7eb552b727e57271fe2
ChanGyunLee <lgyn10@naver.com>
2024-03-17 15:51:07 +0900

    짝수는싫어요: range 함수 활용

--- day23/짝수는싫어요.ts
+++ day23/짝수는싫어요.ts
@@ -1,9 +1,15 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120813
 
+import { range } from 'lodash';
+
 const 짝수는싫어요 = (n: number): number[] => {
   const oddNumbers = [];
   for (let i = 1; i <= n; i += 2) oddNumbers.push(i);
   return oddNumbers;
+};
+
+const 짝수는싫어요2 = (n: number): number[] => {
+  return range(1, n, 2);
 };
 
 const d23n1 = 10;
@@ -11,3 +17,6 @@ const d23n2 = 15;
 
 console.log(짝수는싫어요(d23n1)); // [1, 3, 5, 7, 9]
 console.log(짝수는싫어요(d23n2)); // [1, 3, 5, 7, 9, 11, 13, 15]
+
+console.log(짝수는싫어요2(d23n1)); // [1, 3, 5, 7, 9]
+console.log(짝수는싫어요2(d23n2)); // [1, 3, 5, 7, 9, 11, 13, 15]
