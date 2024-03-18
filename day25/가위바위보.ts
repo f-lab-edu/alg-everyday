7b796e8ef8a7d6b2c04c6a758875e1f688b654ac
ChanGyunLee <lgyn10@naver.com>
2024-03-18 17:07:30 +0900

    가위바위보: 객체에 Record 타입 적용

--- day25/가위바위보.ts
+++ day25/가위바위보.ts
@@ -13,8 +13,25 @@ const 가위바위보 = (rsp: string): string => {
   );
 };
 
+// 객체 사용
+// -> 맵의 데이터 크기가 정적이고, 크지 않을 때
+const 가위바위보2 = (rsp: string): string => {
+  const rspObj: Record<string, string> = {
+    2: '0',
+    0: '5',
+    5: '2',
+  };
+  return join(
+    map(rsp, (v) => rspObj[v]),
+    '',
+  );
+};
+
 const d26rsp1 = '2';
 const d26rsp2 = '205';
 
 console.log(가위바위보(d26rsp1));
 console.log(가위바위보(d26rsp2));
+
+console.log(가위바위보2(d26rsp1));
+console.log(가위바위보2(d26rsp2));
