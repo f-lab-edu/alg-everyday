611daab274f7b740a7387e272e1cc3218345fb18
ChanGyunLee <lgyn10@naver.com>
2024-03-26 13:03:05 +0900

    문자열정렬하기1: 정규식 사용

--- day26/문자열정렬하기1.ts
+++ day26/문자열정렬하기1.ts
@@ -1,12 +1,19 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120850
 
-import { map, parseInt, sortBy, without } from 'lodash';
+import { filter, map, parseInt, sortBy, without } from 'lodash';
 
 const 문자열정렬하기1 = (str: string): number[] => {
   const parseIntStr = map(str, parseInt);
   const onlyNums = without(parseIntStr, NaN);
   const sortedOnlyNums = sortBy(onlyNums);
   return sortedOnlyNums;
+};
+
+// 정규식 사용
+const 문자열정렬하기1ver2 = (str: string): number[] => {
+  const filterNums = filter(str, (v) => /[0-9]/.test(v));
+  const strToNums = map(filterNums, parseInt);
+  return strToNums.sort();
 };
 
 const d28str1 = 'hi12392';
@@ -16,3 +23,7 @@ const d28str3 = 'abcde0';
 console.log(문자열정렬하기1(d28str1)); // [1, 2, 2, 3, 9]
 console.log(문자열정렬하기1(d28str2)); // [2, 2, 4, 8]
 console.log(문자열정렬하기1(d28str3)); // [0]
+
+console.log(문자열정렬하기1ver2(d28str1)); // [1, 2, 2, 3, 9]
+console.log(문자열정렬하기1ver2(d28str2)); // [2, 2, 4, 8]
+console.log(문자열정렬하기1ver2(d28str3)); // [0]
