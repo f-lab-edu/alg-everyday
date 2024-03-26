073970bbab778030dac9e21a477551a1795f556f
ChanGyunLee <lgyn10@naver.com>
2024-03-26 13:03:49 +0900

    약수구하기: 시간 효율 개선
    Math.sqrt(n) 까지만 for문을 돌게 수정

--- day26/약수구하기.ts
+++ day26/약수구하기.ts
@@ -1,11 +1,19 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120897
 
 const 약수구하기 = (n: number): number[] => {
-  const ans = [];
-  for (let i = 1; i <= n; i++) {
-    if (n % i === 0) ans.push(i);
+  const ans1 = [];
+  const ans2 = [];
+  const nSqrt = Math.sqrt(n);
+
+  for (let i = 1; i < nSqrt; i++) {
+    if (n % i === 0) {
+      ans1.push(i);
+      ans2.unshift(n / i);
+    }
   }
-  return ans;
+  if (Number.isInteger(nSqrt)) ans1.push(nSqrt);
+
+  return [...ans1, ...ans2];
 };
 
 const d29n1 = 24;
@@ -13,3 +21,4 @@ const d29n2 = 29;
 
 console.log(약수구하기(d29n1));
 console.log(약수구하기(d29n2));
+console.log(약수구하기(36));
